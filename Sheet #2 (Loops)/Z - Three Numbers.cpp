a87f316868c975c72fd8371163501bf264973e61
Mohamed Nagy <mohmmednagy@gmail.com>
2023-07-10 10:21:41 +0300

    “commit”

---
+++ Sheet #2 (Loops)/Z - Three Numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int max, sum, counter = 0;
+    cin >> max >> sum;
+    for (int i = 0 ; i <= max; i++)
+    {
+        for (int z = 0 ; z <= max; z++)
+        {
+            if (sum - i - z >= 0 && sum - i - z <= max) counter++;
+        }
+    }
+    cout << counter;
+}
