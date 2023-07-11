07e80889885c3cc7d3299a277f72f6a69ec9460d
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-11 20:08:57 +0300

    Add files via upload

---
+++ Sheet #3 (Arrays)/A - Summation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{    
+	long long int counter, sum = 0, cur;
+	cin >> counter;
+	while (counter--)
+	{
+		cin >> cur;
+		sum += cur;
+	}
+	cout << abs(sum);
+}
