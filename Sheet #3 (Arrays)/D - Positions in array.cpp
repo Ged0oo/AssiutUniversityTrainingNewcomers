98643f3032a9ef3e8c19c714b31495a67b898938
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-11 20:11:37 +0300

    Update D - Positions in array.cpp

---
+++ Sheet #3 (Arrays)/D - Positions in array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+	int size;
+	cin >> size;
+
+    int *arr = new int[size];
+    for (int i = 0; i < size; i++) cin >> arr[i];
+
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] <= 10) cout << "A[" << i << "] = " << arr[i] << endl;
+	}
+}
