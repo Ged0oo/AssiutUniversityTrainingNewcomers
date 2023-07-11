7cba5d04269bdff88f966acc309626dacd0f6f4b
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-11 20:09:53 +0300

    Update B - Searching.cpp

---
+++ Sheet #3 (Arrays)/B - Searching.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{   
+	int size;
+	bool flag = false;
+	cin >> size;
+
+    int *arr = new int[size];
+    for (int i = 0; i < size; i++) cin >> arr[i];
+
+	int key;
+	cin >> key;
+	for (int i = 0; i < size; i++)
+	{
+		if (key == arr[i])
+		{
+			flag = true;
+			cout << i;
+			break;
+		}	
+	}
+	if (!flag) cout << -1;
+}
