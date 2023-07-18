49c8f229eb188b484b995ffc0a82d01426cc3b9e
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-18 08:19:17 +0300

    Update O - Fibonacci.cpp

---
+++ Sheet #3 (Arrays)/O - Fibonacci.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    long long int times, first, second, sum;
+    cin >> times;
+    if(times==1) cout << 0 << endl;
+    else if (times == 2) cout << 1 << endl;
+    else
+    {
+        for (int i = 0; i < times; i++)
+        {
+            if (i <= 1)
+            {
+                first = 0;
+                second = 1;
+            }
+            else
+            {
+                sum = first + second;
+                first = second;
+                second = sum;
+            }
+        }
+        cout << sum << endl;
+    }
+}
