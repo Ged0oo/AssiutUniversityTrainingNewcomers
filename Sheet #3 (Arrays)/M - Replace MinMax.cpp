faeeb60d9533cd73cf009c58e1d01eacec4ae2b9
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-18 08:20:22 +0300

    Update M - Replace MinMax.cpp

---
+++ Sheet #3 (Arrays)/M - Replace MinMax.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#define swabInt(x,y) { *y = *y + *x; *x = *y - *x; *y = *y - *x; } 
+
+using namespace std;
+int main()
+{
+	int size;
+	cin >> size;
+	int* arr = new int[size];
+	for (int i = 0; i < size; i++) cin >> arr[i];
+
+	int *mx = max_element(arr, arr + size);
+	int *mn = min_element(arr, arr + size);
+
+	swabInt(mx , mn);
+	for (int i = 0; i < size; i++) cout << arr[i] << " ";	
+}
